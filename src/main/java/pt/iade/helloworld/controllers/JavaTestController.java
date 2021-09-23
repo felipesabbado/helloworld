1b16d95e9ea2aab4ac1d74148407ae39b270b224
Felipe Campelo Sabbado <fsabbado@gmail.com>
2021-09-23 18:30:24 +0100

    JavaTestController
    Java primitive data types and operators and String class exercice

--- src/main/java/pt/iade/helloworld/controllers/JavaTestController.java
+++ src/main/java/pt/iade/helloworld/controllers/JavaTestController.java
@@ -16,11 +16,33 @@ public class JavaTestController {
     @GetMapping(path = "/author", produces = MediaType.APPLICATION_JSON_VALUE)
     public String getAuthor() {
         logger.info("Get author");
-        String name = "Felipe Campelo Sabbado";
-        int number = 20191012;
-        double height = 1.7;
-        
-        return "Name: " + name + "Número: " + number;
+        String name = "Pedrinho Bola";
+        int number = 5462;
+        double height = 1.56;
+        boolean footballFan = true;
+        char fbClubColor = 'R'; // Red, Blue, Green and None
+        String footballClub; // Benfica, Porto, Sporting
+        String footballMsg;
+
+        if(footballFan){
+            if (fbClubColor == 'R'){
+                footballClub = "My favorite club is Benfica.";
+            } else if (fbClubColor == 'B'){
+                footballClub = "My favorite club is Porto.";
+            } else if (fbClubColor == 'G'){
+                footballClub = "My favorite club is Sporting.";
+            } else {
+                footballClub = "I don't have a favorite club.";
+            }
+            footballMsg = "I am a fan of football.\n" + footballClub;
+        } else {
+            footballMsg = "not a fan of football.";
+        }
+
+        String msg = "Done by " + name + " with number " + number + ".\nI am " + height
+        + " tall and " + footballMsg;
+
+        return msg;
     }
 
 }
