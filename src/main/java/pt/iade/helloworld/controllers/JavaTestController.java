ec021139787418ac4de8135ed8604107b95be8ac
Felipe Campelo Sabbado <fsabbado@gmail.com>
2021-10-02 20:10:59 +0100

    Post methods and html

--- src/main/java/pt/iade/helloworld/controllers/JavaTestController.java
+++ src/main/java/pt/iade/helloworld/controllers/JavaTestController.java
@@ -1,12 +1,16 @@
 package pt.iade.helloworld.controllers;
 
+import java.util.ArrayList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pt.iade.helloworld.models.CurricularUnit;
 
 @RestController
 @RequestMapping(path = "/api/java/tester")
@@ -157,5 +161,22 @@ public class JavaTestController {
         }
 
         return msg;
+    }
+
+    // Curricular Unit
+
+    private ArrayList<CurricularUnit> units = new ArrayList<>();
+
+    @PostMapping(path = "/units", produces = MediaType.APPLICATION_JSON_VALUE)
+    public CurricularUnit saveUnit(@RequestBody CurricularUnit unit) {
+        logger.info("Added unit " + unit.getName());
+        units.add(unit);
+        return unit;
+    }
+
+    @GetMapping(path = "/units", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ArrayList<CurricularUnit> getUnits(){
+        logger.info("Get " + units.size() + " units");
+        return units;
     }
 }
